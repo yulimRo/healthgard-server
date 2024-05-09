0acd956b5bdf40a84a42cd305c2be62d68c3e806
ylro <ylro@mcnc>
2024-05-09 22:04:00 +0900

    relaxted #5

--- healthgard-server/src/main/java/com/healthgard/server/web/IndexController.java
+++ healthgard-server/src/main/java/com/healthgard/server/web/IndexController.java
@@ -6,9 +6,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 	
+	/**
+	 * Index Page 
+	 * @return
+	 */
 	@GetMapping("/")
 	public String index() {
-		return "index";
+		return "login";
+	}
+	
+	/**
+	 * Trainer Login Page
+	 * @return
+	 */
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	/**
+	 * Trainer Infomation New Join Page
+	 * @return
+	 */
+	@GetMapping("/join")
+	public String join() {
+		return "join";
 	}
 
 }
