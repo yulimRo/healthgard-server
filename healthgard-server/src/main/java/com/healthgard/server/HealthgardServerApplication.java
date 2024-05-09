037432813e60c748043ffcfd850944782e2bae3b
ylro <ylro@mcnc>
2024-05-09 15:32:19 +0900

    init commit(example springboot project)

---
+++ healthgard-server/src/main/java/com/healthgard/server/HealthgardServerApplication.java
@@ -0,0 +1,13 @@
+package com.healthgard.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthgardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthgardServerApplication.class, args);
+	}
+
+}
