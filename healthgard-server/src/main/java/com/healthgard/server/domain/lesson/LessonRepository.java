d6ceeb27424190289994dad88af619f0ab4d5520
noyulim <yyj2766@naver.com>
2024-05-15 16:52:49 +0900

    Refactor Project Structure

--- healthgard-server/src/main/java/com/healthgard/server/domain/lesson/LessonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.healthgard.server.domain.lesson;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LessonRepository extends JpaRepository<Lesson, Long> {
-	
-}
